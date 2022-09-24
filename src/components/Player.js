2606b113886a6fd784f97a9149c04f0ed7a4bfcb
plcpl-haque <md.haque@profecialinks.com>
2022-09-24 17:49:00 +0600

    player ready

--- src/components/Player.js
+++ src/components/Player.js
@@ -5,8 +5,8 @@ import { Vector3 } from 'three';
 import { useKeyBoard } from '../hooks/useKeyBoard';
 
 const Player = () => {
-    const actions = useKeyBoard();
-    console.log('actions', Object.entries(actions).filter(([k, v]) => v));
+    const { moveForward, moveBackward, moveRight, moveLeft, jump } = useKeyBoard();
+    // console.log('actions', Object.entries(actions).filter(([k, v]) => v));
 
 
     const { camera } = useThree();
@@ -29,7 +29,24 @@ const Player = () => {
     useFrame(() => {
         camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]));
 
-        api.velocity.set(0, 1, 0);
+        const direction = new Vector3();
+
+        const frontVector = new Vector3(0, 0, (moveBackward ? 1 : 0) - (moveForward ? 1 : 0));
+
+        const sideVector = new Vector3((moveLeft ? 1 : 0) - (moveRight ? 1 : 0), 0, 0);
+
+        direction
+            .subVectors(frontVector, sideVector)
+            .normalize()
+            .multiplyScalar(4)
+            .applyEuler(camera.rotation);
+
+        api.velocity.set(direction.x, vel.current[1], direction.z);
+
+        if (jump && Math.abs(vel.current[1] < 0.05)) {
+            api.velocity.set(vel.current[0], 4, vel.current[2]);
+        }
+
     })
 
     return (
